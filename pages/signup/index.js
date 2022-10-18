f77023b2530afc14c13d518be81903be1e3e9d0a
LinneaEriksson <Linnea-E@hotmail.com>
2022-10-18 15:09:53 +0200

    Added sign-up form

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,12 +1,18 @@
 import { supabase } from "../../utils/supabaseClient";
 
 const SignUp = () => {
-  async function testSignUp() {
+  async function testSignUp(event) {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const username = event.target.username.value;
+    const name = event.target.name.value;
+    const password = event.target.password.value;
+
     const { data, error } = await supabase.from("users_nelly").insert({
-      username: "Denmark",
-      name: "Denmark",
-      password: "Denmark",
-      email: "Denmark",
+      username: username,
+      name: name,
+      password: password,
+      email: email,
     });
   }
 
@@ -14,7 +20,37 @@ const SignUp = () => {
     <div>
       <div>
         <h1>Sign Up!</h1>
-        <button onClick={() => testSignUp()}>SIGN UP HERE</button>
+        <form onSubmit={testSignUp}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Write your email here please"
+          ></input>
+          <label htmlFor="email">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Come up with an awesome username and write it here"
+          ></input>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Write your name here"
+          ></input>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Dont put 123 here"
+          ></input>
+          <button type="submit">SIGN UP HERE</button>
+        </form>
       </div>
     </div>
   );
