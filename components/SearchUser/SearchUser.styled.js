87ef4216dde729d2dfb7348cfb821e37c1907228
LinneaEriksson <Linnea-E@hotmail.com>
2022-10-26 09:11:24 +0200

    Added some styling

--- components/SearchUser/SearchUser.styled.js
+++ components/SearchUser/SearchUser.styled.js
@@ -1,9 +1,22 @@
 import styled from "styled-components";
 
-export const FriendDiv = styled.div`
+export const SearchFriendWrapper = styled.div`
   background-color: white;
-  width: 100%;
-  padding: 20px;
+  width: 70%;
+  margin-top: 50px;
+`;
+
+export const FriendDiv = styled.div`
+  background-color: #e1ecdf;
+  width: 90%;
+  padding: 10px;
   border-radius: 10px;
-  margin: 10px;
+  margin: 30px;
+  display: flex;
+  justify-content: space-around;
+  flex-direction: row;
+`;
+
+export const textWrapper = styled.div`
+  justify-content: space-around;
 `;
