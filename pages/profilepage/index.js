818b22d9fbe050c80b7c9d658eb7fb878fa942f9
LinneaEriksson <Linnea-E@hotmail.com>
2022-11-08 10:34:05 +0100

    added rerout

--- pages/profilepage/index.js
+++ pages/profilepage/index.js
@@ -2,9 +2,22 @@ import Menu from "../../components/Menu/Menu";
 import * as GS from "../index.styled";
 import { useSession } from "@supabase/auth-helpers-react";
 import UpdateProfile from "../../components/UpdateProfile/UpdateProfile";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const ProfilePage = () => {
   const session = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    reRout();
+  }, [session]);
+
+  function reRout() {
+    if (!session) {
+      router.push("/");
+    }
+  }
   return (
     <GS.Wrapper>
       <Menu session={session} />
